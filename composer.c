713f0eb14cc74384753bcf86b9d63e66db20a54a
Galen Guyer <galen@galenguyer.com>
2021-04-24 15:40:02 -0400

    Print the error if one occurs

--- composer.c
+++ composer.c
@@ -1,6 +1,10 @@
+#include <stdio.h>
 #include <unistd.h>
 #define COMMAND "echo"
 
 int main(int argc, char** argv) {
-    execvp(COMMAND, argv);
+    int result = execvp(COMMAND, argv);
+    if (result < 0) {
+        perror(COMMAND);
+    }
 }
